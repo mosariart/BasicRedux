06bca05253c2cff532437493903b4a12e9651751
mo__sari <mohsen.ansari.wtf@gmail.com>
2023-07-30 19:01:04 +0430

    creating the reducer

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ function buyCake(){
         info: "First Redux Action"
     }
 }
+const initialState = {
+    numberOfCakes: 10
+}
+const reducer = (state = initialState,action) =>{
+    switch (action.type) {
+        case BUY_CAKE:
+            return {
+                ...state,numberOfCakes: state.numberOfCakes - 1
+            }
+    
+        default:
+            return state;
+    }
+}
