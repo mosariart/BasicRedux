453974a918b983d1b795240f4fd09f064d985904
mo__sari <mohsen.ansari.wtf@gmail.com>
2023-07-30 18:51:54 +0430

    first part of redux, creating an action and actionCreator

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log('From index.js');
-console.log('From index.js');
+const BUY_CAKE = "BUY_CAKE";
+
+function buyCake(){
+    return {
+        type: BUY_CAKE,
+        info: "First Redux Action"
+    }
+}
