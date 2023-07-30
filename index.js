d72a69bd24f5d7c54d2c70acb8a4a38aa6a62c67
mo__sari <mohsen.ansari.wtf@gmail.com>
2023-07-30 20:07:19 +0430

    second way of having multiple reducers

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const redux = require('redux');
 const createStore = redux.createStore;
-
+const combineReducers = redux.combineReducers;
 
 const BUY_CAKE = "BUY_CAKE";
 const BUY_ICECREAM = "BUY_ICECREAM";
@@ -18,16 +18,29 @@ function buyIceCream(){
         info: "First Redux Action"
     }
 }
-const initialState = {
+//this is the second way of having multiple reducers 
+//and in this way we seperated each part of the global
+//store that each reducer is working on
+const cakesState = {
     numberOfCakes: 10,
-    numberOfIceCreams: 20
 }
-const reducer = (state = initialState,action) =>{
+const cakeReducer = (state = cakesState,action) =>{
     switch (action.type) {
         case BUY_CAKE:
             return {
                 ...state,numberOfCakes: state.numberOfCakes - 1
             }
+    
+        default:
+            return state;
+    }
+}
+
+const iceCreamState = {
+    numberOfIceCreams: 20
+}
+const iceCreamReducer = (state = iceCreamState,action) =>{
+    switch (action.type) {
         case BUY_ICECREAM:
             return {
                 ...state,numberOfIceCreams: state.numberOfIceCreams - 1
@@ -37,8 +50,11 @@ const reducer = (state = initialState,action) =>{
             return state;
     }
 }
-
-const store = createStore(reducer);
+const rootReducer = combineReducers({
+    cake:cakeReducer,
+    iceCream: iceCreamReducer
+})
+const store = createStore(rootReducer);
 console.log("initial state",store.getState());
 const unsubscribe = store.subscribe(()=> console.log('updated state',store.getState()));
 store.dispatch(buyCake());
